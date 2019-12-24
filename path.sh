1d66515fc2811c9798d738ba44593527a675ba94
hyliang96 <hyliang96@hotmail.com>
2019-12-24 21:04:05 +0800

    logout push jungpu20 2019-12-24 21:04:05

--- path.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-
-admin_tool_path=$(cd "$(dirname "${BASH_SOURCE[0]-$0}")"; pwd)
-admin_tool_path="`echo ${admin_tool_path} | sed 's/^\/mfs\/\([^\/]\+\)/\/home\/\1\/mfs/'`"
-
-
