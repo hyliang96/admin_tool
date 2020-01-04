c8d7a3187cc355d23280f8f545e9ee3e6143fa7d
hyliang96 <hyliang96@hotmail.com>
2020-01-04 13:48:26 +0800

    logout push jungpu15 2020-01-04 13:48:26

--- mfs_set.sh
+++ mfs_set.sh
@@ -8,27 +8,61 @@ here=$(cd "$(dirname "${BASH_SOURCE[0]-$0}")"; pwd)
 . $here/hosts.sh
 
 
+rmlink()
+{
+    for i in "$@"; do
+        if [ -L "$i" ]; then
+            rm "$i"
+        fi
+    done
+}
+lkdir()
+{
+    if [ -d "$1" ]; then
+        ln -sfT "$1" "$2"
+    fi
+}
+link_to_backup()
+{
+    rmlink /home/${USER}/ENV/{CONF,shareENV,serverENV,junENV}
 
+    lkdir /home/${USER}/ENV/ENV_backup/CONF      /home/${USER}/ENV/CONF
+    lkdir /home/${USER}/ENV/ENV_backup/shareENV  /home/${USER}/ENV/shareENV
+    lkdir /home/${USER}/ENV/ENV_backup/serverENV /home/${USER}/ENV/serverENV
+    lkdir /home/${USER}/ENV/ENV_backup/junENV    /home/${USER}/ENV/junENV
+}
+link_to_mfs()
+{
+    # rmlink /home/${USER}/ENV/{CONF,shareENV,serverENV,junENV}
+
+    lkdir /home/${USER}/mfs/server_conf/ENV/CONF      /home/${USER}/ENV/CONF
+    lkdir /home/${USER}/mfs/server_conf/ENV/shareENV  /home/${USER}/ENV/shareENV
+    lkdir /home/${USER}/mfs/server_conf/ENV/serverENV /home/${USER}/ENV/serverENV
+    lkdir /home/${USER}/mfs/server_conf/ENV/junENV    /home/${USER}/ENV/junENV
+}
 usshmfs() # `usshmfs`：卸载ssh挂载的mfs
 {
     # 本地shareENV、serverENV、CONF链接改指向shareENV_backup、serverENV_backup、CONF_backup
-    [ -L /home/${USER}/ENV/CONF ] && rm /home/${USER}/ENV/CONF
-    [ -L /home/${USER}/ENV/shareENV ] && rm /home/${USER}/ENV/shareENV
-    [ -L /home/${USER}/ENV/serverENV ] && rm /home/${USER}/ENV/serverENV
-    [ -L /home/${USER}/ENV/junENV ] && rm /home/${USER}/ENV/junENV
+    link_to_backup
+    # [ -L /home/${USER}/ENV/CONF ] && rm /home/${USER}/ENV/CONF
+    # [ -L /home/${USER}/ENV/shareENV ] && rm /home/${USER}/ENV/shareENV
+    # [ -L /home/${USER}/ENV/serverENV ] && rm /home/${USER}/ENV/serverENV
+    # [ -L /home/${USER}/ENV/junENV ] && rm /home/${USER}/ENV/junENV
 
-    if [ -d /home/${USER}/ENV/CONF_backup ]; then
-        ln -s /home/${USER}/ENV/CONF_backup /home/${USER}/ENV/CONF
-    fi
-    if [ -d /home/${USER}/ENV/shareENV_backup ]; then
-        ln -s /home/${USER}/ENV/shareENV_backup /home/${USER}/ENV/shareENV
-    fi
-    if [ -d /home/${USER}/ENV/serverENV_backup ]; then
-        ln -s /home/${USER}/ENV/serverENV_backup /home/${USER}/ENV/serverENV
-    fi
-    if [ -d /home/${USER}/ENV/junENV_backup ]; then
-        ln -s /home/${USER}/ENV/junENV_backup /home/${USER}/ENV/junENV
-    fi
+
+    # if [ -d /home/${USER}/ENV/CONF_backup ]; then
+        # ln -s /home/${USER}/ENV/CONF_backup /home/${USER}/ENV/CONF
+    # fi
+    # if [ -d /home/${USER}/ENV/shareENV_backup ]; then
+        # ln -s /home/${USER}/ENV/shareENV_backup /home/${USER}/ENV/shareENV
+    # fi
+    # if [ -d /home/${USER}/ENV/serverENV_backup ]; then
+        # ln -s /home/${USER}/ENV/serverENV_backup /home/${USER}/ENV/serverENV
+    # fi
+    # if [ -d /home/${USER}/ENV/junENV_backup ]; then
+        # ln -s /home/${USER}/ENV/junENV_backup /home/${USER}/ENV/junENV
+    # fi
+
     # 因为HOME=/home/${USER}/ENV/shareENV/CONF
 
     # 如果这个目录不被占用，则卸挂载，无需sudo
@@ -50,7 +84,7 @@ sshmfs_()
     else
         local _mfs_source="$1"
     fi
-
+  
     # 挂载${hostname}上的mfs
     if [ -d /home/${USER}/mfs ]; then
         echo command sshfs $_mfs_source:/mfs/haoyu /home/${USER}/mfs -o allow_other,default_permissions,reconnect
@@ -61,6 +95,7 @@ sshmfs_()
     fi
 
     # # 创建本地linkENV链接，并将本地shareENV链接改指向mfs下的shareENV
+    link_to_mfs
     # if [ -d /home/haoyu/mfs/server_conf/ENV/CONF ]; then
         # ln -sf /home/haoyu/mfs/server_conf/ENV/CONF /home/${USER}/ENV/CONF
     # fi
@@ -128,7 +163,7 @@ allusshmfs()
     else
         local server_set="$1"
     fi
-    sudo su -c ". $admin_tool_path/load.sh && all '$server_set' 'umount -l /home/haoyu/mfs'"
+    sudo su -c ". $admin_tool_path/load.sh && all '$server_set' 'su -l $USER -c \"link_to_backup\" && umount -l /home/haoyu/mfs'"
 }
 
 # 重新用sshfs挂载mfs
