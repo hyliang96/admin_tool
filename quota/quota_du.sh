70a3cc19fa948e43e93f448fc0c3029a139f9034
hyliang96 <hyliang96@hotmail.com>
2020-04-03 12:41:04 +0800

    logout push jungpu9 2020-04-03 12:41:04

--- quota/quota_du.sh
+++ quota/quota_du.sh
@@ -3,5 +3,5 @@
 quota_du() {
   mount_point=$(df /home | awk 'NR!=1' | awk '{print $NF}')
   quota_report="$(sudo repquota -s $mount_point)"
-  { echo "quota of used disk size on $mount_point" && echo $quota_report | awk 'NR<=5' && echo $quota_report | awk 'NR>5' | sort -h -k 3 -r ; } | less
+  { echo "quota of used disk size on $(hostname):$mount_point" && echo $quota_report | awk 'NR<=5' && echo $quota_report | awk 'NR>5' | sort -h -k 3 -r ; } | less
 }
