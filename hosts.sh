1692722bfb355e7fcfff18b7e4bfe677810ac93d
hyliang96 <hyliang96@hotmail.com>
2024-01-05 14:53:41 +0800

    change hosts.sh

--- hosts.sh
+++ hosts.sh
@@ -62,13 +62,23 @@ J5=( "${gJ5[@]}" )
 a=( "${c[@]}" "${g[@]}" )
 
 
-# 本科生或访问学者,只能4卡
-#bks=(jungpu1 jungpu{3..8} jungpu{10..13} jungpu{14..17} jungpu{21..23})
-bks=(jungpu1 jungpu{3..8} jungpu{10..11} jungpu{14..17} jungpu{21..23})
-bks_tmp=(jungpu{14..17})
+
+# 朱老师课程的学生的课程项目用的机器
+kcxm=(jungpu{14..17})
+
+# 本科生或访问学者,只能用4卡机器
+bks=(juncluster{2,4} jungpu1 jungpu{3..8} jungpu{10..11} jungpu{14..17} jungpu{21..23})
+
+# 研究生，能用除了c5、gJ4、gJ5以外所有节点
+yjs=( "${c[@]}" "${gJ1[@]}" "${gJ3[@]}" )
+
+# c5: 专供管理员管理集群, 不对其他用户开放
+# gJ4、gJ5有更高级的显卡，需要单独开账号给受管理员批准的用户
+
+
 
 # 有效编组：即只有写在此处的编组才会被 `all` 命令使用
-server_sets=(gJ1 J1 gJ2 J2 gJ3 J3 gJ4 J4 gJ5 J5 c g a bks bks_tmp)
+server_sets=(gJ1 J1 gJ2 J2 gJ3 J3 gJ4 J4 gJ5 J5 c g a kcxm bks yjs)
 
 
 # # 用不了的gpu
