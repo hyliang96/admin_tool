4af927d9c84219c8b6888deb0c0eb2d3a0c5d5c9
hyliang96 <hyliang96@hotmail.com>
2024-10-06 18:24:30 +0800

    a

--- sl/quota/quota_du.sh
+++ sl/quota/quota_du.sh
@@ -1,7 +1,41 @@
 #!/usr/bin/env zsh
 
 quota_du() {
-  mount_point=$(df /home | awk 'NR!=1' | awk '{print $NF}')
-  quota_report="$(sudo repquota -s $mount_point)"
-  { echo "quota of used disk size on $(hostname):$mount_point" && echo $quota_report | awk 'NR<=5' && echo $quota_report | awk 'NR>5' | sort -h -k 3 -r ; } | less
+    if [ $# -eq 0 ]; then
+        local target_dir="/home"
+    else
+        local target_dir="$1"
+    fi
+    local mount_point=$(df "${target_dir}" | awk 'NR!=1' | awk '{print $NF}')
+    local quota_report="$(sudo repquota -vs $mount_point)"
+
+    local meta_info="${quota_report%%                        Space limits                File limits*}"
+    local no_meta_info="${quota_report#*                        Space limits                File limits}"
+
+    local table=$(
+        echo -n "                        Space limits                File limits"
+        echo -n "${no_meta_info%%Statistics:*}"
+    )
+    local table_head="${table%%----------------------------------------------------------------------*}"
+    local table_line='----------------------------------------------------------------------'
+    local table_body="${table#*----------------------------------------------------------------------}"
+
+    local statistics="Statistics:${no_meta_info#*Statistics:}"
+
+
+    echo "quota of used disk size on $(hostname):$mount_point"
+    echo
+    echo -n $meta_info
+    {
+        echo -n $table_head
+        echo  $table_line
+        echo -n $table_body | sort -rh -k3
+
+    } | sed -E 's/Space limits/|              Space limits/; s/^User      /User      |/; s/^----------/----------|/ ;s/^([^ ]+) +(.+)/\1|\2/' | \
+        column -te -s'|' | \
+        sed -E '/^----------/s/ /-/g'
+
+    echo -n $statistics
 }
+
+alias sl_quota='quota_du'
